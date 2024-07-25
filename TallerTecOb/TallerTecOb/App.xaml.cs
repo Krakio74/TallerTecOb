2ac8f4609faa4d026131b6310a15fcf61aa12fcd
Krakio74 <49844804+Krakio74@users.noreply.github.com>
2024-07-25 20:15:20 -0300

    Main
    Proyecto en blanco

---
+++ TallerTecOb/TallerTecOb/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TallerTecOb
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TallerTecOb/TallerTecOb/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TallerTecOb
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
