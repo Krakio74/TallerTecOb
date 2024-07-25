b1e389a6f85177ad39af982285153c17b126c43d
Krakio74 <49844804+Krakio74@users.noreply.github.com>
2024-07-25 20:44:13 -0300

    Eliminacion duplicados

--- ObligatorioTTec/ObligatorioTTec/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/ObligatorioTTec.AssemblyInfo.cs
+++ ObligatorioTTec/ObligatorioTTec/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/ObligatorioTTec.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ObligatorioTTec")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+fae5934cebb0862c69b2ed233b2b71cdc6858d83")]
 [assembly: System.Reflection.AssemblyProductAttribute("ObligatorioTTec")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ObligatorioTTec")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
